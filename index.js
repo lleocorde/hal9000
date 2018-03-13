0fb4784b3d41276713fc4b63b6d9d16551d58ab3
lleocorde <32246923+lleocorde@users.noreply.github.com>
2018-03-12 19:59:04 -0700

    Role testing
    Added lines to determine role of message poster and validate.

--- index.js
+++ index.js
@@ -9,16 +9,29 @@ bot.on('ready', () => {
 });
 
 bot.on('message',(message) => {
-  if (message.author.bot) return; // Ignore bots.
-  if (message.channel.type === "dm") {//return; // Ignore DM channels.
-      message.channel.send('I\'m not setup for DMs yet...');
+  if(message.author.bot) return; // Ignore bots.
+  if(message.channel.type === "dm") {//return; // Ignore DM channels.
+    message.channel.send('I\'m not setup for DMs yet...');
+    return;
   }
-  //if(message.content.startsWith('!')) {
+
   if(message.content == '!test') {
-    message.channel.send(
-      '\`Hal is online and responding.\`'
-    );
+    message.channel.send('\`Hal is online and responding.\`');
+    return;
   }
+  
+  if(message.content.startsWith('!')) {
+    //message.channel.send('Your name is: '+message.author.username);
+ /*
+    if(message.member.roles.some(r=>["Dev", "Mod", "Server Staff", "Proficient"].includes(r.name)) ) {
+      message.channel.send(message.author.username+' has role *Lunatic Prime*');
+    } else {
+      message.channel.send(message.author.username+' does not have correct role.');
+    }
+*/
+    let primeR = message.guild.roles.get(process.env.lPrime);
+    if(message.member.roles.has(primeR.id)) {message.channel.send(message.author.username+' has role'+primeR);}
+}
 });
 
 bot.login(process.env.DISCORD_BOT_TOKEN);
