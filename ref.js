745cb50a1286e40cdf1363f96fb6d140139282fc
lleocorde <32246923+lleocorde@users.noreply.github.com>
2018-03-13 18:23:25 -0700

    Creating reference file for external items.

---
+++ ref.js
@@ -0,0 +1,13 @@
+module.exports.bankJoin = (usr) => '__**'+usr+'** has joined The Galactic Bank__\r\n\r\n'
+                             +'Please make your initial deposit using the \`!bank deposit\` command.\r\n'
+                             +'\`\`\`\r\n'
+                             +'Examples:\r\n'
+                             +'!bank deposit [type] [level] [quantity]\r\n'
+                             +'!bank deposit tet 2 3\r\n'
+                             +'!bank deposit orb 4 2\r\n'
+                             +'!bank deposit crystal 3 1\r\n'
+                             +'\`\`\`';
+module.exports.bankNull = 'The Galactic Bank records the artifact cache of it\'s members and coordinates trade.\r\n\r\n'
+                             +'*You are seeing this informational message because you used the \`!bank\` request without a specific command.*'
+                             +' Try \`!bank help\` for instructions.';
+module.exports.bankHelp = 'This help file is incomplete...';
